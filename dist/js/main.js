6dd0186f9e3f74c5a7ea65ff55000403d5b4622a
lvan226 <lvan226@list.ru>
2020-02-05 14:17:44 +0800

    Added contract, conf and fix many errors

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,26 +13,36 @@ function ready() {
 
   // Анимация чисел JavaScript
   function timer(field, second, num, numhow) {
-		var _Seconds = $(field).text(),
-			int;
-		_Seconds = 0;
-		int = setInterval(function () { // запускаем интервал
-			if (_Seconds < num) {
-				if (_Seconds == num - 1) {
-					
-				}
-				_Seconds += numhow; // прибавляем 1
-				$(field).text(_Seconds); // выводим получившееся значение в блок
-			} else {
-				clearInterval(int); // очищаем интервал, чтобы он не продолжал работу при _Seconds = 0
-			}
-		}, second);
+    var _Seconds = $(field).text(),
+      int;
+    _Seconds = 0;
+    int = setInterval(function () { // запускаем интервал
+      if (_Seconds < num) {
+        if (_Seconds == num - 1) {
+
+        }
+        _Seconds += numhow; // прибавляем 1
+        $(field).text(_Seconds); // выводим получившееся значение в блок
+      } else {
+        clearInterval(int); // очищаем интервал, чтобы он не продолжал работу при _Seconds = 0
+      }
+    }, second);
   }
   timer('#animNumber1', 8, 6549, 10);
   timer('#animNumber2', 9, 3743, 6);
   timer('#animNumber3', 30, 2000, 10);
   timer('#animNumber4', 800, 4, 1);
 
+  // Табы
+  $('.tab-title > div').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('.tab-title > div').removeClass('current');
+		$('.tab-content__item').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 
   // Модальные окна
   function modalPopUp() {
@@ -70,28 +80,27 @@ function submitHandler(e) {
   // var request = new XMLHttpRequest();
   // var th = document.querySelector('.contact__form-thanks');
 
-  request.onreadystatechange = function() { 
+  request.onreadystatechange = function () {
     // console.log("readyState=", this.readyState, "statis=", this.status);
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
       // Выполнится когда форма успешно отправлена
-        // success, show this.responseText here
-        // console.log("SUCCESS", this);
-        // th.classList.add('contact__form-thanks_active');
-        // form.setAttribute('disabled', 'disabled');
+      // success, show this.responseText here
+      // console.log("SUCCESS", this);
+      // th.classList.add('contact__form-thanks_active');
+      // form.setAttribute('disabled', 'disabled');
     }
   }
-  
+
   request.open(this.method, this.action, true);
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  
+
   var data = new FormData(this);
   var dataPost;
   // Формируем массив данных для отправки 
-  data.forEach(function(value, key) {
+  data.forEach(function (value, key) {
     dataPost += '&' + key + '=' + value;
   })
   // console.log(data);
-    
+
   request.send(dataPost);
 }
-
