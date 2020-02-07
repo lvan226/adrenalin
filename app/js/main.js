0a1c36a8e899d3a9c4e8fa167a8c5c966dd746f0
lvan226 <lvan226@list.ru>
2020-02-07 16:28:19 +0800

    quiz

--- app/js/main.js
+++ app/js/main.js
@@ -34,15 +34,109 @@ function ready() {
   timer('#animNumber4', 800, 4, 1);
 
   // Табы
-  $('.tab-title > div').click(function(){
-		var tab_id = $(this).attr('data-tab');
+  $('.tab-title > div').click(function () {
+    var tab_id = $(this).attr('data-tab');
 
-		$('.tab-title > div').removeClass('current');
-		$('.tab-content__item').removeClass('current');
+    $('.tab-title > div').removeClass('current');
+    $('.tab-content__item').removeClass('current');
 
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
+    $(this).addClass('current');
+    $("#" + tab_id).addClass('current');
+  })
+
+  let input_change = $('#change_input');
+  if(input_change.length) {
+    console.log('1', input_change);
+    alert('324234');
+    input_change.on('keyup', function() {
+      let radio = document.querySelector('#howmuch5');
+      radio.value = input_change.val();
+    });
+  }
+
+  // Квиз
+  let quizID = 1;
+  let arrQuiz = document.querySelectorAll('[data-quiz]');
+  let quizLine = document.querySelector('.quiz__line-complete');
+  let quizLineWidth = 100 / arrQuiz.length;
+  let quizLineComplete = quizLineWidth;
+  let quizStart = document.querySelector('.quiz__info-start');
+  let quizEnd = document.querySelector('.quiz__info-end');
+  let quizStartNumber = 0;
+  let quizPrice = document.querySelector('.quiz__cost-number input');
+  let quizPriceNumber = Number(quizPrice.value);
+  let isQuiz = true;
+  quizPrice.innerHTML = quizPriceNumber;
+  quizStart.innerHTML = quizStartNumber;
+  quizEnd.innerHTML = arrQuiz.length - 1;
+
+  $('.quiz__info-next').on('click', () => {
+    if (quizID < arrQuiz.length) {
+      $('[data-quiz=' + quizID + ']').removeClass('current');
+      quizID++;
+      $('[data-quiz=' + quizID + ']').addClass('current');
+      quizLineComplete += quizLineWidth;
+      quizStartNumber++;
+      quizStart.innerHTML = quizStartNumber;
+      quizLine.style.width = quizLineComplete + "%";
+    }
+    if (isQuiz) {
+      let intervalQuiz = setInterval(function() {
+        quizPrice.value = quizPriceNumber--;
+        if(Number(quizPrice.value) == 100 ) {clearInterval(intervalQuiz);}
+      }, 1000)
+      isQuiz = false;
+    }
+  })
+
+  $('.quiz__info-prev').on('click', () => {
+    if (quizID > 1) {
+      $('[data-quiz=' + quizID + ']').removeClass('current');
+      quizID--;
+      $('[data-quiz=' + quizID + ']').addClass('current');
+      quizLineComplete -= quizLineWidth;
+      quizStartNumber--;
+      quizStart.innerHTML = quizStartNumber;
+      quizLine.style.width = quizLineComplete + "%";
+    }
+  })
+
+  let mainblockBtn = document.querySelector('.mainblock__btn').addEventListener('click', function() {
+    $('.quiz-item .jq-radio').change(function() {
+      disableBtn();
+    });
+    $('.quiz__info-next').on('click', function() {
+      disableBtn();
+    })
+    $('.quiz__info-prev').on('click', function() {
+      disableBtn();
+    });
+  });
+
+  // radioAll[0].addEventListener('click', function() {
+  //   disableBtn();
+  // });
+ 
+  function disableBtn() {
+    let radioList = document.querySelectorAll('.quiz-item.current .jq-radio');
+    let isChecked = false;
+    // console.log(radioList);
+    for(let i=0; i<radioList.length; i++) {
+        if( radioList[i].classList.contains('checked') ) { isChecked = true; break; }
+    }
+    if( isChecked ) {
+      // alert("Разблокировано");
+      $('.quiz__info-next').removeAttr('disabled');
+    }
+    else {
+      $('.quiz__info-next').attr('disabled', 'disabled');
+    }
+  }
+
+  // Formstyle
+  $('input[type=radio]').styler({
+
+  });
 
   // Модальные окна
   function modalPopUp() {
@@ -60,7 +154,7 @@ function ready() {
     for (let i = 0; i < popup.length; i++) {
       popup[i].addEventListener('click', () => {
         popup[i].parentNode.classList.remove('active');
-        overlay.classList.toggle('overlay_active');
+        overlay.classList.remove('overlay_active');
       });
     }
   }
@@ -77,12 +171,25 @@ function ready() {
 function submitHandler(e) {
   e.preventDefault();
   // var form = this.querySelector('button');
-  // var request = new XMLHttpRequest();
+  var form = document.querySelector('#quiz')
+  var result = '<div class="quiz-item current quiz-item-success" data-quiz="8g"><div class="quiz__title">Сообщение отправлено</div>';
+  result += '<div class="quiz-img"></div><div class="quiz__text">Спасибо! Мы свяжемся с вами в ближайшее время</div>';
+  result += '<div class="quiz__okey popup__close">ОК</div></div><div class="quiz__close popup__close popup-close"></div>'
+       
+       
+      
+  var request = new XMLHttpRequest();
   // var th = document.querySelector('.contact__form-thanks');
 
   request.onreadystatechange = function () {
     // console.log("readyState=", this.readyState, "statis=", this.status);
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+
+      form.innerHTML = result;
+      $('.popup__close').on('click', function() {
+       $('.quiz').removeClass('active');
+       overlay.classList.remove('overlay_active');
+      });
       // Выполнится когда форма успешно отправлена
       // success, show this.responseText here
       // console.log("SUCCESS", this);
--- app/order-landing.php
+++ app/order-landing.php
@@ -1,9 +1,14 @@
 <?php
 // ini_set('display_errors',1);
 // error_reporting(E_ALL);
-$name = trim($_POST["userName"]);
-$email = trim($_POST["userEmail"]);
-$text_message = trim($_POST["userText"]);
+$name = trim($_POST["quizName"]);
+$phone = trim($_POST["quizPhone"]);
+$quiz_event = trim($_POST["radio1"]);
+$quiz_men = trim($_POST["radio2"]);
+$quiz_age = trim($_POST["radio3"]);
+$quiz_platform = trim($_POST["platform"]);
+$quiz_time = trim($_POST["quizTime"]);
+$quiz_discount = trim($_POST["quizDiscount"]);
 
 var_dump($_POST);
 
@@ -11,20 +16,41 @@ echo '1';
 // send an email
 $to = 'lvan226@yandex.ru';
  
-$subject = 'Заявка с Ainaru';
- 
+$subject = 'Заявка с Adrenalin';
+
+// $arrMess = foreach ($_POST as $key => $value) {
+//   if($value != "") {
+
+//   }
+// }
+$c = true;
+foreach ( $_POST as $key => $value ) {
+  if ( $value != "") {
+    $message .= "
+    " . ( ($c = !$c) ? '<div>':'<div style="background-color: #f8f8f8;">' ) . "
+      <p style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b><i style='padding-left: 10px;'>        $value</i></p>
+    </div>
+    ";
+  }
+}
+// $message = $arrMess;
 // message
-$message = '
-<html>
-<head>
-<title>Заявка с сайта Adrenalin</title>
-</head>
-<body>
-<p>Заявка с сайта</p>
-<p>Имя: ' . $name . '</p>
-<p>Е-маил: ' . $email . '</p>
-<p>Текст сообщения: ' . $text_message . '</p>
-';
+// $message = '
+// <html>
+// <head>
+// <title>Заявка с сайта Adrenalin</title>
+// </head>
+// <body>
+// <p>Заявка с сайта adrenalin. Квиз форма</p>
+// <p>Имя пользователя: ' . $name . '</p>
+// <p>Телефон: ' . $phone . '</p>
+// <p>Размер скидки: ' . $quiz_discount . '</p>
+// <p>Мероприятие: ' . $quiz_event . '</p>
+// <p>Сколько человек: ' . $quiz_men . '</p>
+// <p>Средний возраст участников: ' . $quiz_age . '</p>
+// <p>Площадка: ' . $quiz_platform . '</p>
+// <p>Дата проведения: ' . $quiz_time . '</p>
+// ';
  
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-Type: text/html; charset=utf-8' . "\r\n";
